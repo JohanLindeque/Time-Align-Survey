4881d52c954ad003a69aca70cdb8fb9d22845d2c
Johan Lindeque <johanlindeque34@gmail.com>
2026-02-13 23:13:58 +0200

    added ResultComparisonDto

--- TimeAlignSurvey/Models/DTO/ResultComparisonDto.cs
+++ TimeAlignSurvey/Models/DTO/ResultComparisonDto.cs
@@ -4,5 +4,13 @@ namespace TimeAlignSurvey.Models.DTO;
 
 public class ResultComparisonDto
 {
+public string QuestionName { get; set; }
 
+    public decimal ActualAverage { get; set; }
+
+    public decimal Objective { get; set; }
+
+    public decimal Gap { get; set; }
+
+    public decimal Accuracy { get; set; }
 }
